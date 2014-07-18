b34e95870dee04cf06f87d41e4bc54a8abb1a1b7
Jovlang <johannes.a.langoy@gmail.com>
2014-07-18 17:32:04 +0200

    update

--- static/script.js
+++ static/script.js
@@ -1,3 +1,18 @@
+function reverseString(str)
+{
+  var result = "";
+  for (var i = str.length-1; i >= 0; i--)
+  {
+    result += str[i];
+  }
+  return result;
+}
+
+function reverseEachLine()
+{
+  return $("#buffer")[0].value.split("\n").map(reverseString).join("\n");
+}
+
 window.onload = function()
 {
   $("#open")[0].onchange = function(event)
@@ -7,6 +22,14 @@ window.onload = function()
     reader.onload = function()
     {
       $("#buffer")[0].value = reader.result;
-    };
-  };
+    }
+  }
+  $("#rev_whole")[0].onclick = function(event)
+  {
+    $("#buffer")[0].value = reverseString($("#buffer")[0].value);
+  }
+  $("#rev_lines")[0].onclick = function(event)
+  {
+    $("#buffer")[0].value = reverseEachLine($("#buffer")[0].value);
+  }
 }
